3283fbbdf7a3027a159c9640fe152e101be39082
S. M. Abdul Aziz <abdulaziz.aziz99@gmail.com>
2019-10-18 19:36:22 +0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 500) {
+        $("#MainNav").addClass("NavbarBg");
+    } else {
+        $("#MainNav").removeClass("NavbarBg");
+    }
+});
+
+//Preloader
+var preloader = $('.preloader');
+$(window).on('load', function () {
+    var preloaderFadeOutTime = 300;
+
+    function hidePreloader() {
+        preloader.fadeOut(preloaderFadeOutTime);
+    }
+    hidePreloader();
+});
